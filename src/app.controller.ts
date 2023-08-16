278e1c6343a1fed4b0318ba709d35e3987c2cf4e
Grigory Telenok <telenok.grigory@gmail.com>
2023-08-16 14:28:13 +1000

    Add handshake endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,17 @@
 import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
   @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req: any) {
